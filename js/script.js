87319f5578cce4f6f145566ce386196c82b1e178
unknown <smarcus4@gmu.edu>
2018-09-12 11:54:06 -0400

    first push

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// load in effects for canvas
+$("#hello").addClass("animated fadeIn");
+$("#myName").addClass("animated fadeIn delay-1s");
+$("#canvasMe").addClass("animated fadeIn delay-2s");
+$("#info").addClass("animated fadeIn delay-3s");
+
