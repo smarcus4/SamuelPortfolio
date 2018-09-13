a5292cbfd2ae1db8b791bc8f4ea4ad990e9dc22a
unknown <smarcus4@gmu.edu>
2018-09-13 17:59:30 -0400

    nav bar working

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $("#hello").addClass("animated fadeIn");
 $("#myName").addClass("animated fadeIn delay-1s");
 $("#canvasMe").addClass("animated fadeIn delay-2s");
 $("#info").addClass("animated fadeIn delay-3s");
+$("#canvasBtn").addClass("animated fadeIn delay-3s");
+
+
+
+//rotate the arrow on canvas
+$('.btn-lg').hover(function () {
+    $(this).children('#arrow').toggleClass('down');
+});
+
 
